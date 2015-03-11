af64ccdf07a17e7a23b9b470d724cede63cef6df
shawntellemoncy <smoncy@engr-134-08.eastern.ewu.edu>
2015-03-11 13:56:14 -0700

    Images

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,16 @@
+var cheney = '99004';
+var spokane = '99220';
+var temp = weather.temp;
+var city = weather.city;
+var state = weather.region;
+var thumb = weather.thumbnail;
 
 $(document).ready(function() {
   $.simpleWeather({
     location: 'Cheney, WA',
     woeid: '',
     unit: 'f',
+    
     success: function(weather) {
       html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
       html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
@@ -18,10 +25,15 @@ $(document).ready(function() {
   });
 });
 
-var cheney = '99004';
-var spokane = '99220';
-var temp = weather.temp;
-var city = weather.city;
-var state = weather.region;
-var thumb = weather.thumbnail;
+// if ('weather.currently' = 'fair') {
+//   html.addClass('.partlycloudy')
+// } else {
+//   html.removeClass('.sunny' + ',' + '.cloudy')
+// };
+
+
+
+
+
+
 
